97e07fe0eb0642775e91ca1a2c118076d1669958
yangzq12 <yangzq12@users.noreply.github.com>
2023-02-13 17:20:37 +0800

    remove transferFrom

--- test.sol
+++ test.sol
@@ -89,12 +89,4 @@ contract CodeToken is ERC20Interface, SafeMath {
         
         return true;
     }
-
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-        balances[from] = safeSub(balances[from], tokens);
-        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
-        balances[to] = safeAdd(balances[to], tokens);
-        emit Transfer(from, to, tokens);
-        return true;
-    }
 }
