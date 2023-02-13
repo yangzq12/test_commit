9739e3863eb53974526054ce8211e45ef9e8b3f1
yangzq12 <yangzq12@users.noreply.github.com>
2023-02-13 17:19:34 +0800

    Update test.sol

--- test.sol
+++ test.sol
@@ -85,6 +85,8 @@ contract CodeToken is ERC20Interface, SafeMath {
         balances[msg.sender] = safeSub(balances[msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
         emit Transfer(msg.sender, to, tokens);
+        
+        
         return true;
     }
 
