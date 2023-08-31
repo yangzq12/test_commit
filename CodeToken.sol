48778e10e9235eab3eeef583e5b2f61171b83cf1
Stone <yangzq12@users.noreply.github.com>
2023-08-31 16:39:06 +0800

    Update CodeToken.sol

--- CodeToken.sol
+++ CodeToken.sol
@@ -84,6 +84,8 @@ contract CodeToken is ERC20Interface, SafeMath {
     function transfer(address to, uint tokens) public returns (bool success) {
         balances[msg.sender] = safeSub(balances[msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
+
+
         emit Transfer(msg.sender, to, tokens);
         return true;
     }
